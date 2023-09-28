ebcf056a1abc35e3dd1a10f11585237ed1110db0
Efimov-Alex <fastic2018l@yandex.ru>
2023-09-28 11:35:23 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    Server server = new Server();
+
+    server.add_connection();
+
+
+
+
+  }
+}
+
+
